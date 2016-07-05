16ace60c8ad17e1e3ab77a01eced1a0f90ed681b
Andrew DeLacy <alcleland@gmail.com>
2016-07-05 15:46:02 -0700

    wrote drawCheckerBoard() method, action bar still shows. Not perfect screen use yet.

--- app/src/main/java/april/bustabobble/MainActivity.java
+++ app/src/main/java/april/bustabobble/MainActivity.java
@@ -1,6 +1,7 @@
 package april.bustabobble;
 
 import android.app.Activity;
+import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.*;
@@ -10,6 +11,7 @@ import android.view.*;
 import java.util.Random;
 
 public class MainActivity extends Activity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +30,11 @@ public class MainActivity extends Activity {
         public DrawView(Context context) {
             super(context);
 
+
             // Get the screen size before the main canvas is ready
             DisplayMetrics metrics = new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            screenSize = new Point(metrics.widthPixels-20, metrics.heightPixels-20);
+            screenSize = new Point(metrics.widthPixels, metrics.heightPixels-20);
 
             // Create the back buffer
             bufferBitmap = Bitmap.createBitmap(screenSize.x, screenSize.y, Bitmap.Config.ARGB_8888);
@@ -54,50 +57,8 @@ public class MainActivity extends Activity {
         public void drawOnBuffer() {
             Paint paint = new Paint();
             paint.setAntiAlias(true);
-
-            // Clear the buffer with color
-            bufferCanvas.drawColor(Color.WHITE );
-
-            // Draw random circles
-            for (int n=0; n<500; n++) {
-                // Make random color
-                int r = rand.nextInt(256);
-                int g = rand.nextInt(256);
-                int b = rand.nextInt(256);
-                paint.setColor(Color.rgb(r, g, b));
-
-                // Make a random position and radius
-                int x = rand.nextInt(bufferCanvas.getWidth());
-                int y = rand.nextInt(bufferCanvas.getHeight());
-                int radius = rand.nextInt(100) +20;
-
-                // Draw one circle
-                bufferCanvas.drawCircle(x, y, radius, paint);
-
-            }
+            drawCheckerBoard(paint);
         }
-        public void displayMetricsDemo(Canvas canvas, Paint paint, DisplayMetrics dm) {
-            // Get canvas resolution
-            int width = canvas.getWidth();
-            int height = canvas.getHeight();
-
-            // Print out screen details
-            int x = 10, y = 30, step = 36;
-            canvas.drawText("Density: " + dm.density, x, y, paint);
-            y += step;
-            canvas.drawText("Scaled density: " + dm.scaledDensity, x, y, paint);
-            y += step;
-            canvas.drawText("Width " + width, x, y, paint);
-            y += step;
-            canvas.drawText("Height " + height, x, y, paint);
-            y += step;
-            canvas.drawText("X dpi: " + dm.xdpi, x, y, paint);
-            y += step;
-            canvas.drawText("Y dpi: " + dm.ydpi, x, y, paint);
-
-        } // displayMetricsDemo()
-
-
 
         public void drawStuff(Canvas canvas, Paint paint, Random random) {
             // Draw some circles with random colors in random locations
@@ -106,15 +67,6 @@ public class MainActivity extends Activity {
                 canvas.drawCircle(Math.abs(100+random.nextInt())%400, 20+Math.abs(random.nextInt())%400, 50, paint);
             }
 
-            // Draw a line
-            paint.setColor(Color.BLACK);
-            canvas.drawLine(0,0,400,400, paint);
-
-            // Draw a box using the method definied here.
-            Point p1 = new Point(20,20);
-            Point p2 = new Point(100,100);
-            this.drawBox(canvas, p1, p2, paint);
-
             //Draw a box using Canvas method
             Point p3 = new Point(50,50);
             Point p4 = new Point(200,200);
@@ -122,14 +74,46 @@ public class MainActivity extends Activity {
             canvas.drawRect(p3.x, p3.y, p4.x, p4.y, paint);
         } //drawStuff()
 
-        // Method for drawing box (superfluous b/c drawRect is already a method in Canvas)
-        public void drawBox(Canvas canvas, Point p1, Point p2, Paint paint) {
-            canvas.drawLine(p1.x, p1.y, p1.x, p2.y, paint);
-            canvas.drawLine(p1.x, p1.y, p2.x, p1.y, paint);
-            canvas.drawLine(p1.x, p2.y, p2.x, p2.y, paint);
-            canvas.drawLine(p2.x, p1.y, p2.x, p2.y, paint);
-        }
 
-        //
+        // Draw a b
+        public void drawCheckerBoard(Paint paint) {
+            //Paint paint = new Paint();
+
+            // Clear the buffer with color
+            bufferCanvas.drawColor(Color.WHITE);
+
+            // Determine the number of squares on the board
+            int NUM_SQUARES_X = 6;
+            int NUM_SQUARES_Y = (int) ((float)screenSize.y/screenSize.x * NUM_SQUARES_X);
+            bufferCanvas.drawText("NUM_SQUARES: " + NUM_SQUARES_X + ", " + NUM_SQUARES_Y, 10, 30, paint);
+
+            // Determine square size
+            int sideLength = Math.round((float) screenSize.x/NUM_SQUARES_X);
+
+            // Set square colors and set to FILL
+            int color1 = (Color.BLACK);
+            int color2 = (Color.WHITE);
+            paint.setStyle(Paint.Style.FILL);
+
+
+            // Draw the checker board
+            int temp = color1;
+            for (int row = 0; row < NUM_SQUARES_Y; row++) {
+                for (int col = 0; col < NUM_SQUARES_X; col++) {
+                    if (col % 2 == 0) {
+                        paint.setColor(color1);
+                    }
+                    else {
+                        paint.setColor(color2);
+                    }
+                    bufferCanvas.drawRect(col*sideLength, row*sideLength, col*sideLength + sideLength, row*sideLength + sideLength, paint);
+                }
+                // Swap starting color on new row.
+                temp = color1;
+                color1 = color2;
+                color2 = temp;
+            }
+        } // drawCheckerBoard()
+
     } //DrawView
 } //MainActivity
